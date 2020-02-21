c7bbdd0da043ab67991782342b96a34fb7255707
David Zhang <59639140+davidzhangg@users.noreply.github.com>
2020-02-21 00:14:06 -0800

    first commit

---
+++ contact.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $(".submit").click(function(event) {
+    var email = $(".email").val();
+    var subject = $(".subject").val();
+    var message = $(".message").val();
+    var statusElm = $(".status");
+
+    statusElm.empty();
+
+    if (email.length < 5 || !email.includes("@") || !email.includes(".")) {
+      event.preventDefault();
+      statusElm.append("<div>* Email is not valid *</div>");
+    }
+
+    if (subject.length < 2) {
+      event.preventDefault();
+      statusElm.append("<div>* Subject is not valid *</div>");
+    }
+
+    if (message.length < 10) {
+      event.preventDefault();
+      statusElm.append("<div>* Message is not valid *</div>");
+    }
+  });
+});
